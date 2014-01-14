bf2219a7967c83419fa9445f5c0b1a7b93d97e0a
alanjkg <alanjkg@gmail.com>
2014-01-14 13:24:07 -0500

    exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,17 @@
+#tip
+amount = 55
+new_amount = amount * 0.20
+puts "the tip amount is #{new_amount}"
+
+
+#adding_string_and_integer
+
+puts "Alan" + 24.to_s
+
+#calculator
+a=45628
+b=7839
+puts "multiplying #{a} and #{b} is #{a * b}"
+
+#boolean
+(true && false) || (false && true) || !(false && false)
